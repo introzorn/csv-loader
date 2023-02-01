2cf8ad6d87130dec72ad1f6fe0a9cadddd06f827
introzorn <introzorn@yandex.ru>
2023-02-01 19:34:37 +0700

    parse csv done

--- Models/csv_files.php
+++ Models/csv_files.php
@@ -1,48 +1,60 @@
 <?php
 
 namespace App\Models;
+
 use App;
 use App\Model as M;
 use App\Contracts\CSV_Parser;
-class csv_files extends App\Model{
+
+class csv_files extends App\Model
+{
     // public $CHARSET='utf8mb4'; 
     // public $COLLATE='unicode_ci';
 
     public function MIGRATE() //миграции 
     {
-    
+
         $this->TABLE = [
-        'id' => 'int(11) NOT NULL AUTO_INCREMENT',
-        'slug' => 'char(255) NOT NULL',
-        'name' => 'char(255) NOT NULL',
-        'description' => 'text(1080)',
-        'PRIMARY KEY'=>'id',
-        'CHARSET'=>'utf8'];
+            'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+            'slug' => 'char(255) NOT NULL',
+            'name' => 'char(255) NOT NULL',
+            'description' => 'text(1080)',
+            'PRIMARY KEY' => 'id',
+            'CHARSET' => 'utf8'
+        ];
     }
 
 
-    public function addCSV($file){
+    public function addCSV($file)
+    {
+   
+        $scv = new CSV_Parser();
+       
+        if ($scv->load($file["tmp_name"]) === false) {
+         
+            return false;
+        }
 
-        $scv= new CSV_Parser();
-        if( $scv->load($file["tmp_name"])===false){return false;}
+        echo ('<table>');
+$i=0;
+        $scv->parse(function ($csv_asoc, $csv_data)use(&$i) {
+             echo ("<tr><td>");
+            echo (join("</td><td>", $csv_data));
+            echo ("</td></tr>");
+           $i++;
+        });
 
-        $scv->parse(function($csv_asoc){echo("<pre>");var_dump($csv_asoc);echo("</pre>");});
+        echo ('</table>');
 
-die();
+        echo($i);
+        die;
 
-        $id=$this->add(["name"=>$file["name"],"slug"=>""]);
-        $this->edit($id,["slug"=>dechex($id)]);
+        $id = $this->add(["name" => $file["name"], "slug" => ""]);
+        $this->edit($id, ["slug" => dechex($id)]);
 
-      
+
 
 
         return true;
     }
-
-
 }
-
-
-
-
-
